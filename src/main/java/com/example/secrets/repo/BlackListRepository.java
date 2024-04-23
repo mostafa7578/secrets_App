55bfd82a476f8f1e7e1f8a69ff74bd8e77e49385
Mostafa Mahmoud <mostafa19500mahmoud@gmail.com>
2024-04-23 18:42:54 +0200

    Add @Repository annotation to BlackListRepository and LoginUsers

--- src/main/java/com/example/secrets/repo/BlackListRepository.java
+++ src/main/java/com/example/secrets/repo/BlackListRepository.java
@@ -3,9 +3,11 @@ package com.example.secrets.repo;
 import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import model.BlackList;
 
+@Repository
 public interface BlackListRepository extends MongoRepository<BlackList, String>{
 
     Optional<BlackList> findByEmail(String email);
--- src/main/java/com/example/secrets/repo/LoginUsers.java
+++ src/main/java/com/example/secrets/repo/LoginUsers.java
@@ -3,9 +3,11 @@ package com.example.secrets.repo;
 import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import model.LoginUser;
 
+@Repository
 public interface LoginUsers extends MongoRepository<LoginUser, String>{
 
     Optional<LoginUser> findByOtp(String otp);
