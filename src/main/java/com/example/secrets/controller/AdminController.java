ad98f61f0f91ab9ac050a49de2b80e18d43841d9
Mostafa Mahmoud <mostafa19500mahmoud@gmail.com>
2024-05-17 21:45:50 +0300

    chore: Update AdminController to use request parameters instead of request body for OTP operations

--- src/main/java/com/example/secrets/controller/AdminController.java
+++ src/main/java/com/example/secrets/controller/AdminController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.secrets.service.AdminService;
@@ -40,12 +41,12 @@ public class AdminController {
     }
 
     @PostMapping("/otp")
-    public String otp(@RequestBody String new_otp) {
+    public String otp(@RequestParam String new_otp) {
         return adminService.otp(new_otp);
     }
 
     @DeleteMapping("/otp")
-    public String deleteOtp(@RequestBody String deleted_otp) {
+    public String deleteOtp(@RequestParam String deleted_otp) {
         return adminService.deleteOtp(deleted_otp);
     }
 }
